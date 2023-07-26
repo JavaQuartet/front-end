23b0ca8bc24b379153145018e53e80a611e9e86f
pqpq0420 <pqpq0420@gmail.com>
2023-07-26 15:51:06 +0900

    Create navbar component

--- src/App.js
+++ src/App.js
@@ -13,55 +13,6 @@ import "./stylesheet/App.scss";
 function App() {
     return (
         <div className="App">
-            <div className="Navbar">
-                <span>
-                    <p>
-                        <Link
-                            to="/"
-                            style={{ textDecoration: "none", color: "#42D084" }}
-                        >
-                            plowithme
-                        </Link>
-                    </p>
-                </span>
-                <div className="main-list">
-                    <ul>
-                        <li>
-                            <Link
-                                to="/club"
-                                style={{
-                                    textDecoration: "none",
-                                    color: "#3EB977",
-                                }}
-                            >
-                                모임
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                to="/community"
-                                style={{
-                                    textDecoration: "none",
-                                    color: "#3EB977",
-                                }}
-                            >
-                                커뮤니티
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                to="/myPage"
-                                style={{
-                                    textDecoration: "none",
-                                    color: "#3EB977",
-                                }}
-                            >
-                                마이페이지
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
             <Routes>
                 <Route path="/" element={<Main />} />
                 <Route path="/club" element={<Club />} />
