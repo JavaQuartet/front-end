4e4e10b6d0689c2df16d5e052f6f225a86ace332
seungJoo <sj10230506@gmail.com>
2023-07-29 17:01:20 +0900

    update navbar

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ import Community from "./pages/community.js";
 import MyPage from "./pages/myPage.js";
 import Main from "./pages/main.js";
 import Settings from "./pages/settings.js"
+import Navbar from "./components/navbar.js"
+import MyPloggings from "./pages/myPloggings.js";
 
 //스타일시트 파일
 import "./stylesheet/App.scss";
-import MyPloggings from "./pages/myPloggings.js";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/club" element={<Club />} />
         <Route path="/community" element={<Community />} />
-        <Route path="/" element={<MyPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="myploggings" element={<MyPloggings />} />
       </Routes>
     </div>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,55 +6,49 @@ import "../stylesheet/navbar.scss";
 
 function Navbar() {
     return (
-        <div className="Nav-background">
-            <div className="Navbar">
-                <span>
-                    <p>
+        <div className="Navbar">
+            <p>
+            <Link
+                to="/"
+                style={{ textDecoration: "none", color: "#42D084" }}
+            >plowithme
+            </Link></p>
+            <div className="main-list">
+                <ul>
+                    <li>
                         <Link
-                            to="/"
-                            style={{ textDecoration: "none", color: "#42D084" }}
+                            to="/club"
+                            style={{
+                                textDecoration: "none",
+                                color: "#3EB977",
+                            }}
                         >
-                            plowithme
+                            모임
                         </Link>
-                    </p>
-                </span>
-                <div className="main-list">
-                    <ul>
-                        <li>
-                            <Link
-                                to="/club"
-                                style={{
-                                    textDecoration: "none",
-                                    color: "#3EB977",
-                                }}
-                            >
-                                모임
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                to="/community"
-                                style={{
-                                    textDecoration: "none",
-                                    color: "#3EB977",
-                                }}
-                            >
-                                커뮤니티
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                to="/myPage"
-                                style={{
-                                    textDecoration: "none",
-                                    color: "#3EB977",
-                                }}
-                            >
-                                마이페이지
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                    </li>
+                    <li>
+                        <Link
+                            to="/community"
+                            style={{
+                                textDecoration: "none",
+                                color: "#3EB977",
+                            }}
+                        >
+                            커뮤니티
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/myPage"
+                            style={{
+                                textDecoration: "none",
+                                color: "#3EB977",
+                            }}
+                        >
+                            마이페이지
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </div>
     );
