f1c761fe2e2e8e6e38071549984fb692927da36b
seungJoo <sj10230506@gmail.com>
2023-09-03 16:04:23 +0900

    update login function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
 //자체작성 페이지 or 컴포넌트
@@ -16,6 +16,14 @@ import SignUp from "./pages/signUp.js";
 //스타일시트 파일
 import "./stylesheet/App.scss";
 function App() {
+
+    let [user, setUser] = useState({
+        isLogin:false,
+        id: '',
+        email: '',
+        name: ''
+    });
+
     return (
         <div className="App">
             <Navbar />
@@ -24,10 +32,10 @@ function App() {
                 <Route path="/" element={<Main />} />
                 <Route path="/club" element={<Club />} />
                 <Route path="/community" element={<Community />} />
-                <Route path="/mypage" element={<MyPage />} />
+                <Route path="/mypage" element={<MyPage user={user} />} />
                 <Route path="/myploggings" element={<MyPloggings />} />
                 <Route path="/settings" element={<Settings />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/login" element={<Login setUser={setUser}/>} />
                 <Route path="/signup" element={<SignUp />} />
             </Routes>
         </div>
